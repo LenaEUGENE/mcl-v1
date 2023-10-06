e22e983a7e01577f87a65e29183dab534ba0bd11
Lena EUGENE <lenaeugene@gmail.com>
2023-10-06 17:27:38 +0200

    add gallery to presntation page

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,7 +17,18 @@ const pricing = defineCollection({
   }),
 });
 
+const gallery = defineCollection({
+  schema: z.object({
+    mediaType: z.enum(["Image", "Vidéo"]),
+    video: z.string(). optional(),
+    image: z.string(). optional(),
+    alt: z.string(),
+    title: z.string(),
+    description: z.string(),
+  }),
+});
+
 export const collections = {
-  pricing,
+  pricing, gallery
 }
 // 3. Export a single `collections` object to register your collection(s) 
---
+++ src/content/gallery/1_transmartinique_short_1.md
@@ -0,0 +1,7 @@
+---
+mediaType: Vidéo
+video: https://www.youtube.com/embed/3VRKIBOtS8g
+alt: something
+title: "La TransMartinique"
+description: "Massage sur l'étape Saint-Joseph de la TransMartinique et ses 134km de course relais."
+---
