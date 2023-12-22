2436943e8c3bc89f1babc0d6cea4425521d0b8df
Lena EUGÈNE <lenaeugene@gmail.com>
2023-12-22 10:04:40 +0100

    Revert "matching Kinsta template"
    This reverts commit 2e277450159fc4defe005003e1701dff5b39c1a1.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import sitemap from '@astrojs/sitemap';
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
   output: "server",
   adapter: vercel()
 });
