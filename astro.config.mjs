cd64ab172567b56d48e8351d31fc87a0459561b5
Lena EUGÈNE <lenaeugene@gmail.com>
2023-12-23 11:40:33 +0100

    add sharp and correct path to images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: squooshImageService(),
+  },
   integrations: [tailwind()],
   output: "server",
   adapter: vercel()
